48abf12be285331d18f8d42d90257a46a022df7b
Missael Cortes <missaelcorm@gmail.com>
2023-10-10 01:54:36 +0000

    Allowing just 2 cards in flipping state

--- Sesion_16/main.js
+++ Sesion_16/main.js
@@ -1,47 +1,46 @@
 $(document).ready(() => {
-    
+    const ROWS = 5;
+    const COLS = 4;
+    const minValue = 0;
+    const maxValue = ROWS * COLS - 1;
+    const FLIP_TIME = 1000;
+    const uniqueIntegers = Array.from({ length: maxValue - minValue + 1 }, (_, i) => i + minValue);
+
     Handlebars.registerHelper('getImg', value => {
         const images = {};
-        
-        for (let i = 0; i < COLS*ROWS; i++) {
-            images[i] = (i > maxValue/2) ? maxValue - i : i;
+
+        for (let i = 0; i < COLS * ROWS; i++) {
+            images[i] = (i > maxValue / 2) ? maxValue - i : i;
         }
-        
+
         return images[value];
     });
-
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
 
     // Create a shuffled array of unique integers
     function shuffleArray(arr) {
         for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
         }
     }
 
-    function flip(card){
+    function flip(card) {
         $(card).removeClass("not-flipped");
-        $(card).addClass("flipped"); 
+        $(card).addClass("flipped");
     }
 
-    function unflip(card){
+    function unflip(card) {
         $(card).removeClass("flipped");
-        $(card).addClass("not-flipped"); 
+        $(card).addClass("not-flipped");
     }
 
-    const ROWS = 5;
-    const COLS = 4;
-    const minValue = 0;
-    const maxValue = ROWS*COLS-1;
-    
-    const uniqueIntegers = Array.from({ length: maxValue - minValue + 1 }, (_, i) => i + minValue);
+    function checkForWin() {
+        if (flippedCards.length === maxValue + 1) {
+            alert("Congratulations, You Won! :D");
+        }
+    }
 
-    shuffleArray(uniqueIntegers);
-
-    function fillArrayRandomly(matrix, numRows, numCols, uniqueIntegers){
+    function fillArrayRandomly(matrix, numRows, numCols, uniqueIntegers) {
         let currentIndex = 0;
 
         for (let i = 0; i < numRows; i++) {
@@ -53,14 +52,15 @@ $(document).ready(() => {
                 matrix.rows[i].cols[j] = uniqueIntegers[currentIndex++];
             }
         }
-        
+
     }
 
     const gridTemplate = $("#template").first();
     const gridContainer = $(".container").first();
-
     const template = Handlebars.compile(gridTemplate.html());
     
+    shuffleArray(uniqueIntegers);
+
     const matrix = {
         rows: new Array(ROWS),
         title: "Memorama"
@@ -72,56 +72,44 @@ $(document).ready(() => {
     gridContainer.html(gridResult);
     const cards = $(".card");
 
-    var cardsFlippedCount = 0;
     const currentCards = [];
     const flippedCards = [];
+    let isFlipping = false;
 
-    cards.each((index, card) => {
-        $(card).on("click", () => {
-            currentCards.push(card);
-            
-            if($(card).hasClass("not-flipped")){
-                cardsFlippedCount++;
-                flip(card);
+    cards.on("click", (event) => {
+        const card = event.currentTarget;
 
-                if(cardsFlippedCount == 2){
-                    let firstCard = parseInt($(currentCards[0]).attr('data-card'), 10);
-                    let secondCard = parseInt($(currentCards[1]).attr('data-card'), 10);
+        // Ignore clicks on already flipped cards or during a flip
+        if ($(card).hasClass("flipped") || isFlipping) {
+            return;
+        }
 
-                    // console.log(firstCard, secondCard, "=", (firstCard+secondCard));
+        currentCards.push(card);
+        flip(card);
 
-                    if(firstCard+secondCard===19){
-                        $(currentCards[0]).addClass("card-found");
-                        $(currentCards[1]).addClass("card-found");
+        if (currentCards.length === 2) {
+            isFlipping = true;
 
-                        // console.log("WIN");
+            setTimeout(() => {
+                let firstCard = parseInt($(currentCards[0]).data('card'), 10);
+                let secondCard = parseInt($(currentCards[1]).data('card'), 10);
 
-                        flippedCards.push(currentCards[0]);
-                        flippedCards.push(currentCards[1]);
-
-                        currentCards.splice(0, currentCards.length);
-                        cardsFlippedCount = 0;
-                    } else {
-                        currentCards.forEach(card => {
-                            setTimeout(() => {
-                                unflip(card);
-                            }, 1000);
-                        });
-
-                        currentCards.splice(0, currentCards.length);
-                        cardsFlippedCount = 0;
-                    }
+                if (firstCard + secondCard === maxValue) {
+                    currentCards.forEach(card => {
+                        $(card).addClass("card-found");
+                        flippedCards.push(card);
+                    });
+                } else {
+                    currentCards.forEach(card => {
+                        setTimeout(unflip, FLIP_TIME, card);
+                    });
                 }
 
-            } else if($(card).hasClass("flipped") && !$(card).hasClass("card-found")){
-                unflip(card);
-            } 
-
-            if(flippedCards.length===maxValue+1){
-                alert("Congratulations, You Won! :D");
-            }
-        });
-        
+                currentCards.splice(0, currentCards.length);
+                isFlipping = false;
+                checkForWin();
+            }, FLIP_TIME);
+        }
     });
-    
-})
+
+});
