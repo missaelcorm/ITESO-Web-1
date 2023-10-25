272a5b91ccd624bc7db52a2c9c7650d1597b69ae
Missael Cortes <missaelcorm@gmail.com>
2023-10-25 18:06:26 +0000

    Fixing MongoDB Password leaked

--- Sesion_17-20/app.js
+++ Sesion_17-20/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 const routes = require('./routes');
 
-const mongoPass = 'om9GMj5erhyTN4Hc';
-const mongoUrl = `mongodb+srv://robertocortes:${mongoPass}@cluster0.a2ffsre.mongodb.net/todos?retryWrites=true&w=majority`;
+const {
+    MONGO_PROTOCOL,
+    MONGO_HOST,
+    MONGO_DB,
+    MONGO_USER,
+    MONGO_PASS,
+} = process.env;
+
+const MONGO_URI = `${MONGO_PROTOCOL}://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_DB}?retryWrites=true&w=majority`;
 
 app.use('', routes);
 
@@ -14,7 +22,7 @@ app.get('', (req, res) => {
     res.send("Request received lol");
 });
 
-mongoose.connect(mongoUrl).then((client) => {
+mongoose.connect(MONGO_URI).then((client) => {
     app.listen(8080, () => {
         console.log('Server running on port 8080');
     });
